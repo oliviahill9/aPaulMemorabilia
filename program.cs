a941a1af7ff64665441ffce25b7a8d45ff7226a0
OFLH <oliviahill99992@gmail.com>
2021-09-27 15:17:42 +0100

    this is my first commit

---
+++ program.cs
@@ -0,0 +1,124 @@
+using System;
+
+namespace ExquisiteCorpse
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      BuildACreature("ghost", "monster", "bug"); 
+    }
+
+    static void BuildACreature(string head, string body, string feet) {
+      
+      switch (head)
+       {
+        case "ghost":
+          GhostHead();
+          break;
+        
+        case "bug":
+          BugHead();
+          break;
+
+        case "monster":
+          MonsterHead();
+          break;
+       }
+
+
+       switch (body)
+       {
+         case "ghost":
+           GhostBody();
+           break;
+
+         case "bug":
+           BugBody();
+           break;
+
+         case "monster":
+           MonsterBody();
+           break;
+       }
+
+       switch (feet)
+       {
+         case "ghost":
+           GhostFeet();
+           break;
+
+         case "bug":
+           BugFeet();
+           break;
+
+         case "monster":
+           MonsterFeet();
+           break;
+       }
+    }
+
+    static void GhostHead()
+    {
+      Console.WriteLine("     ..-..");
+      Console.WriteLine("    ( o o )");
+      Console.WriteLine("    |  O  |");
+    }
+
+    static void GhostBody()
+    {
+      Console.WriteLine("    |     |");
+      Console.WriteLine("    |     |");
+      Console.WriteLine("    |     |");
+    }
+
+    static void GhostFeet()
+    {
+      Console.WriteLine("    |     |");
+      Console.WriteLine("    |     |");
+      Console.WriteLine("    '~~~~~'");
+    }
+
+    static void BugHead()
+    {
+      Console.WriteLine("     /   \\");
+      Console.WriteLine("     \\. ./");
+      Console.WriteLine("    (o + o)");
+    }
+
+    static void BugBody()
+    {
+      Console.WriteLine("  --|  |  |--");
+      Console.WriteLine("  --|  |  |--");
+      Console.WriteLine("  --|  |  |--");
+    }
+
+    static void BugFeet()
+    {
+      Console.WriteLine("     v   v");
+      Console.WriteLine("     *****");
+    }
+
+    static void MonsterHead()
+    {
+      Console.WriteLine("     _____");
+      Console.WriteLine(" .-,;='';_),-.");
+      Console.WriteLine("  \\_\\(),()/_/");
+      Console.WriteLine("　  (,___,)");
+    }
+
+    static void MonsterBody()
+    {
+      Console.WriteLine("   ,-/`~`\\-,___");
+      Console.WriteLine("  / /).:.('--._)");
+      Console.WriteLine(" {_[ (_,_)");
+    }
+
+    static void MonsterFeet()
+    {
+      Console.WriteLine("    |  Y  |");
+      Console.WriteLine("   /   |   \\");
+      Console.WriteLine("   \"\"\"\" \"\"\"\"");
+    }
+  }
+}
